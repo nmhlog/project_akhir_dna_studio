aa9b33f30e670355c054d63a5f5001f7d9e1122b
nmhlog <naufalhirzi@gmail.com>
2025-09-16 20:30:45 +0700

    perubahan di dag_naufal_download_extract.py

--- dags/dag_naufal_download_extract.py
+++ dags/dag_naufal_download_extract.py
@@ -9,24 +9,24 @@ import zipfile
 def download_file_from_gdrive():
     # Link download langsung dari Google Drive
     file_id = Variable.get("file_id_naufal", default_var="zVER3BGOcF4URiIebR_nBB8faZnYY4yP")
-    output = Variable.get("output", default_var="dags/data/download_file.zip")
+    output_file_zip = Variable.get("output_file_zip", default_var="dags/data/download_file.zip")
     url = f"https://drive.google.com/uc?export=download&id={file_id}"
 
     # Pastikan folder tujuan ada
-    os.makedirs(os.path.dirname(output), exist_ok=True)
+    os.makedirs(os.path.dirname(output_file_zip), exist_ok=True)
 
     # Download pakai requests
     response = requests.get(url, stream=True)
     if response.status_code == 200:
-        with open(output, "wb") as f:
+        with open(output_file_zip, "wb") as f:
             for chunk in response.iter_content(1024):
                 f.write(chunk)
-        print(f"File berhasil diunduh ke: {output}")
+        print(f"File berhasil diunduh ke: {output_file_zip}")
     else:
         print(f"Gagal download, status code: {response.status_code}")
         
 def unzip_in_same_folder():
-    input_zip = Variable.get("output", default_var="dags/data/download_file.zip")
+    input_zip = Variable.get("output_file_zip", default_var="dags/data/download_file.zip")
 
     # folder asal file zip
     output_dir = os.path.dirname(input_zip)
@@ -38,7 +38,7 @@ def unzip_in_same_folder():
     
 def delete_download_file():
     
-    input_zip = Variable.get("output", default_var="dags/data/download_file.zip")
+    input_zip = Variable.get("output_file_zip", default_var="dags/data/download_file.zip")
 
     if os.path.exists(input_zip):
         os.remove(input_zip)
